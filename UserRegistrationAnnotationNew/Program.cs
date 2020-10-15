a4d368199eb6674135a5b984f4992d797bf12ea0
Lokendra <lokilokendra12345@gmail.com>
2020-10-15 22:24:30 +0530

    [lokendra] first commit

---
+++ UserRegistrationAnnotationNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationAnnotationNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" welcome to userAnnotation ");
+        }
+    }
+}
