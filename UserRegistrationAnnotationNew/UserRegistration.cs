2f7caca67da29ec8a975a003335632359fa85842
Lokendra <lokilokendra12345@gmail.com>
2020-10-16 08:41:53 +0530

    [lokendra]Refactor a code to uc3

--- UserRegistrationAnnotationNew/UserRegistration.cs
+++ UserRegistrationAnnotationNew/UserRegistration.cs
@@ -10,14 +10,16 @@ namespace UserRegistrationAnnotations
         [Required(ErrorMessage = "Username {0} is required")]
         [StringLength(30, MinimumLength = 3, ErrorMessage = "Firstname starts with capital letter and should have minimum 3 charcters")]
         [DataType(DataType.Text)]
+        [RegularExpression(@"^[A-z]{1}[a-zA-Z]{2,}$")]
         public string FirstName { get; set; }
         [Required(ErrorMessage = "Username {0} is required")]
         [StringLength(30, MinimumLength = 3, ErrorMessage = "Lastname starts with capital letter and should have minimum 3 charcters")]
         [DataType(DataType.Text)]
+        [RegularExpression(@"^[A-z]{1}[a-zA-Z]{2,}$")]
         public string LastName { get; set; }
         [Required (ErrorMessage = "EmailId is required {0}")]
         [DataType(DataType.EmailAddress)]
-        [EmailAddress]
+        [RegularExpression ("^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$")]
         public string EmailId { get; set; }
         /*[Required (ErrorMessage = "phone number is required {0}")]
         [DataType(DataType.PhoneNumber)]
